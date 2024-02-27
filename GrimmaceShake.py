7b89675bd70bdd71e4d32a76f3edc06a3f360966
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-02-27 13:45:04 -0500

    Used Time Library to Pause Inbetween Print Statements - Stable

--- GrimmaceShake.py
+++ GrimmaceShake.py
@@ -19,29 +19,36 @@ def vehicleResponseSystem():
     if weatherAlert == "snowy":
         print("\nNational Weather Service has updated our alarm by 30 minutes because of the forecast of",weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 50mph.")
     elif weatherAlert == "blizzard":
         print("\nNational Weather Service has updated our alarm by 45 minutes because of the forecast of", weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 40mph.")
     elif weatherAlert == "rainy":
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 60mph.")
     elif weatherAlert == "foggy":
         print("\nNational Weather Service has updated our alarm by 15 minutes because of the forecast of", weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 55mph.")
     elif weatherAlert == "icy":
         print("\nNational Weather Service has updated our alarm by 60 minutes because of the forecast of", weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 30mph.")
     elif weatherAlert == "windy":
         print("\nNational Weather Service has updated our alarm by 5 minutes because of the forecast of", weatherAlert,
               "weather conditions")
+        sleep(1.5)
         print("VRS has been only allowing you to drive 70mph.")
     else:
         print("\nYou do not need to worry about weather, it is a", weatherAlert,"day out today")
+        sleep(1.5)
         print("VRS has been disengaged")
 
 
