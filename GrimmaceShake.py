ff73cbfb71fa498837f6a2218eff58c8673c3d70
Maxwell Moss <maxwellmoss332009@gmail.com>
2024-02-27 13:40:27 -0500

    Updated VRS Function with ELIF and ELSE - Stable

--- GrimmaceShake.py
+++ GrimmaceShake.py
@@ -8,7 +8,7 @@ from time import sleep
 
 #Create a function that randomly chooses the weather from a list
 def weather():
-    weatherForcast = ["snowy", "blizzards", "rainy", "foggy", "windy", "icy", "sunny", ]
+    weatherForcast = ["snowy", "blizzard", "rainy", "foggy", "windy", "icy", "sunny", ]
     weatherConditions = random.choice(weatherForcast)
     return weatherConditions
 
@@ -28,6 +28,21 @@ def vehicleResponseSystem():
         print("\nNational Weather Service has updated our alarm by 10 minutes because of the forecast of", weatherAlert,
               "weather conditions")
         print("VRS has been only allowing you to drive 60mph.")
+    elif weatherAlert == "foggy":
+        print("\nNational Weather Service has updated our alarm by 15 minutes because of the forecast of", weatherAlert,
+              "weather conditions")
+        print("VRS has been only allowing you to drive 55mph.")
+    elif weatherAlert == "icy":
+        print("\nNational Weather Service has updated our alarm by 60 minutes because of the forecast of", weatherAlert,
+              "weather conditions")
+        print("VRS has been only allowing you to drive 30mph.")
+    elif weatherAlert == "windy":
+        print("\nNational Weather Service has updated our alarm by 5 minutes because of the forecast of", weatherAlert,
+              "weather conditions")
+        print("VRS has been only allowing you to drive 70mph.")
+    else:
+        print("\nYou do not need to worry about weather, it is a", weatherAlert,"day out today")
+        print("VRS has been disengaged")
 
 
 vehicleResponseSystem()
